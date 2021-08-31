ae4260c466772b7a71bd38e181b9192541099db7
Wesley Lübke <88849772+wesleylubke@users.noreply.github.com>
2021-08-31 14:54:18 -0300

    Add files via upload

---
+++ aula10/microDesafio.js
@@ -0,0 +1,16 @@
+let lista = ['arroz', 'feijão', 'azeite']
+console.log(lista)
+    //
+console.log('Retira o primeiro elemento to array')
+lista.shift()
+    //
+console.log('Adiciona um elemento no início do array')
+lista.unshift('macarrão')
+    //
+console.log('Retira o último elemento do array')
+lista.pop()
+    //
+console.log('Adiciona um elemento no final do array')
+lista.push('farinha de trigo')
+    //
+console.log(lista.join(' - '))
